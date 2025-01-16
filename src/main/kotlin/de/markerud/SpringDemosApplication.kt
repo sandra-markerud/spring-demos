03c244e2becd0f16a05011b951f0a628c73340fc
Markerud Sandra <sandra.markerud@x-tention.de>
2025-01-16 09:39:36 +0100

    initial commit

---
+++ src/main/kotlin/de/markerud/SpringDemosApplication.kt
@@ -0,0 +1,11 @@
+package de.markerud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringDemosApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringDemosApplication>(*args)
+}
---
+++ src/test/kotlin/de/markerud/SpringDemosApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.markerud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDemosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
